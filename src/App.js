de5fd2b972dc83a461afe9c1e5eff5ae42e8d1a9
Maxycarp89 <maxicostilla21@gmail.com>
2022-12-28 00:40:44 -0300

    add responsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import About from "./components/About";
+import Experience from "./components/Experience";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
 
 
@@ -8,6 +11,9 @@ function App() {
     <div>
      <NavBar />
      <Home />
+    <About />
+    <Portfolio />
+    <Experience/>
 
      <SocialLinks />
     </div>
